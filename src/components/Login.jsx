0556d9968f70f64699a6afe9decb46e71faa71b0
Subhankar Dutta <duttasubhankar999@gmail.com>
2024-04-02 16:18:01 +0530

    update the login jsx file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,62 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-  const [isLogin, setIsLogin] = useState(false);
-  const [isError, setIsError] = useState(false);
+  let [userName, setUserName] = useState("");
+  let [password, setPassword] = useState("");
+  let [success, setSuccess] = useState(false);
+  let [toggle, setToggle] = useState(false);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData.username);
-    console.log(formData.password);
-    let username = formData.username;
-    let password = formData.password;
-    if (username.trim() === "user" && password.trim() === "password") {
-      setIsLogin(!isLogin);
+    if (e.target.name === "username") {
+      setUserName(e.target.value);
     } else {
-      setIsError(!isError);
+      setPassword(e.target.value);
     }
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateInput(userName, password)) return;
+
+    if (userName !== "user" && password !== "password") {
+      setSuccess(true);
+    }
+
+    if (userName === "user" && password === "password") {
+      setToggle(true);
+    }
+  };
+
+  const validateInput = (user, password) => {
+    if (user.trim === "" || user === undefined) {
+      return false;
+    }
+    if (password.trim === "" || password === undefined) {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
-      {isLogin ? (
-        <p>Welcome user!</p>
+      {toggle ? (
+        <p>Welcome, user!</p>
       ) : (
-        <div>
-          {isError && <p>Invalid username or password</p>}
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="">Username:</label>
-            <input
-              type="text"
-              onChange={handleChange}
-              placeholder="username"
-              name="username"
-              required
-            />
-            <br />
-            <label htmlFor="">Password:</label>
-            <input
-              type="password"
-              onChange={handleChange}
-              placeholder="password"
-              name="password"
-              required
-            />
-            <br />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+        <form onSubmit={handleSubmit}>
+          {success && <p>Invalid username or password</p>}
+          <label>Username:</label>
+          <input
+            type="text"
+            value={userName}
+            name="username"
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <br />
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       )}
     </div>
   );
